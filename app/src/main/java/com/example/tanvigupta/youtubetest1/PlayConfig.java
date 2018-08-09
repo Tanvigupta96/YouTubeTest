282da728c6e6e6712c1091dbda74d652ff20c549
Mayank <mayank_kush@wedmegood.com>
2018-08-09 13:46:15 +0530

    formatted code

--- app/src/main/java/com/example/tanvigupta/youtubetest1/PlayConfig.java
+++ app/src/main/java/com/example/tanvigupta/youtubetest1/PlayConfig.java
@@ -2,5 +2,5 @@ package com.example.tanvigupta.youtubetest1;
 
 public class PlayConfig {
 
-    public final  static String API_KEY="AIzaSyBjuFXsFKH8dvFjTOE1PFj8BBAO-9Wisf8";
+    public final static String API_KEY = "AIzaSyBjuFXsFKH8dvFjTOE1PFj8BBAO-9Wisf8";
 }
