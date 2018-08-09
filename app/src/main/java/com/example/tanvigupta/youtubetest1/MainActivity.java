45bbfe488f72da76cedacfc399d5c0c80b4b7eaf
Tanvi Gupta <tanvigupta@Tanvis-MacBook-Air.local>
2018-08-09 13:29:47 +0530

    initial commit

---
+++ app/src/main/java/com/example/tanvigupta/youtubetest1/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.tanvigupta.youtubetest1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+    YouTubePlayerView youTubePlayerView;
+    Button button;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        youTubePlayerView=(YouTubePlayerView)findViewById(R.id.youtube_player);
+        button=(Button)findViewById(R.id.button);
+
+        onInitializedListener=new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("Qsn7w0WS7HQ");
+                youTubePlayer.play();
+
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                youTubePlayerView.initialize(PlayConfig.API_KEY,onInitializedListener);
+            }
+        });
+
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/tanvigupta/youtubetest1/PlayConfig.java
@@ -0,0 +1,6 @@
+package com.example.tanvigupta.youtubetest1;
+
+public class PlayConfig {
+
+    public final  static String API_KEY="AIzaSyBjuFXsFKH8dvFjTOE1PFj8BBAO-9Wisf8";
+}
