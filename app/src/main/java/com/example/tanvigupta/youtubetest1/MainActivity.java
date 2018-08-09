f87bbbca67b60a274ffc5ef2c796043d6a96c24a
Mayank <mayank_kush@wedmegood.com>
2018-08-09 13:43:06 +0530

    added youtube video jar

--- app/src/main/java/com/example/tanvigupta/youtubetest1/MainActivity.java
+++ app/src/main/java/com/example/tanvigupta/youtubetest1/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.tanvigupta.youtubetest1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
@@ -15,41 +13,30 @@ public class MainActivity extends YouTubeBaseActivity {
     Button button;
     YouTubePlayer.OnInitializedListener onInitializedListener;
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        youTubePlayerView=(YouTubePlayerView)findViewById(R.id.youtube_player);
-        button=(Button)findViewById(R.id.button);
+        youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_player);
+        button = (Button) findViewById(R.id.button);
 
-        onInitializedListener=new YouTubePlayer.OnInitializedListener() {
-            @Override
-            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                YouTubePlayer youTubePlayer, boolean b) {
                 youTubePlayer.loadVideo("Qsn7w0WS7HQ");
                 youTubePlayer.play();
-
             }
 
-            @Override
-            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+            @Override public void onInitializationFailure(YouTubePlayer.Provider provider,
+                YouTubeInitializationResult youTubeInitializationResult) {
 
             }
         };
 
         button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                youTubePlayerView.initialize(PlayConfig.API_KEY,onInitializedListener);
+            @Override public void onClick(View v) {
+                youTubePlayerView.initialize(PlayConfig.API_KEY, onInitializedListener);
             }
         });
-
-
-
-
-
-
-
     }
 }
